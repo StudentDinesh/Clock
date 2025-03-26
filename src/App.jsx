566ae27bb9bbdad68426fda03a75ea938970c9f9
Dinesh <dinesh807227@gmail.com>
2025-03-26 22:26:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css';
+import Stopwatch from './Component/Stopwatch';
+
+const App = () => {
+  return (
+    <div>
+      <Stopwatch/>
+    </div>
+  )
+}
+
+export default App
